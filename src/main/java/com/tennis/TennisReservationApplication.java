26872a9da09b383604e93d3bb9b71fe99b09b376
jn4624 <jn4624@gmail.com>
2024-10-30 01:38:57 +0900

    프로젝트 패키지 구조 변경

--- src/main/java/com/tennis/TennisReservationApplication.java
+++ src/main/java/com/tennis/TennisReservationApplication.java
@@ -1,4 +1,4 @@
-package com.example.tennisreservation;
+package com.tennis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tennis/TennisReservationApplicationTests.java
+++ src/test/java/com/tennis/TennisReservationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tennisreservation;
+package com.tennis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
